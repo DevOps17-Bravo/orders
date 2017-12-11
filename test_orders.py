182c1213bbaab7aa03af6ceca7bda87e16adae60
Qinxin <qinxin.tan@nyu.edu>
2017-12-11 16:07:38 -0500

    change of test_orders file

--- test_orders.py
+++ test_orders.py
@@ -29,7 +29,7 @@ class TestOrders(unittest.TestCase):
     def test_create_an_order(self):
         # Create an order and assert that it exists
         order = Order(0, "321", 999, "98765")
-        self.assertTrue( order != None )
+        self.assertNotEqual(Order, None)
         self.assertEqual( order.order_id, 0 )
         self.assertEqual( order.customer_id, "321" )
         self.assertEqual( order.order_total, 999 )
@@ -47,23 +47,28 @@ class TestOrders(unittest.TestCase):
         self.assertEqual( order.order_id, 1 )
         orders = Order.all()
         self.assertEqual( len(orders), 1)
+        self.assertEqual(orders[0].order_id, 1)
+        self.assertEqual(orders[0].customer_id, "321")
+        self.assertEqual(orders[0].order_total, 999)
+        self.assertEqual(orders[0].order_time, "98765")
 
     def test_update_an_order(self):
         order = Order(0, "54321", 9999, "98765" )
         order.save()
-        self.assertEqual( order.customer_id, "54321")
+        self.assertEqual( order.order_id, 1)
         # Change order_total an save it
         order.order_total = 100
         order.save()
-        self.assertEqual( order.order_total, 100)
+        self.assertEqual( order_id, 1)
         # Fetch it back and make sure the id hasn't changed
         # but the data did change
         orders = Order.all()
         self.assertEqual( len(orders), 1 )
         self.assertEqual( orders[0].order_total, 100 )
+        self.assertEqual( orders[0].customer_id, "54321")
 
     def test_delete_an_order(self):
-        order = Order(0, "", 0, "")
+        order = Order(0, "123", 0, "321")
         order.save()
         self.assertEqual( len(Order.all()), 1 )
         # delete the order and make sure it isn't in the database
@@ -84,44 +89,46 @@ class TestOrders(unittest.TestCase):
         self.assertEqual( data["order_time"], "98765" )
 
     def test_deserialize_an_order(self):
-        data = {"order_id": 0, "customer_id": "321", "order_total": 999, "order_time": "98765", "order_status": 1}
-        order = Order()
+        data = {"order_id": 1, "customer_id": "321", "order_total": 999, "order_time": "98765", "order_status": 1}
+        order = Order(data["order_id"])
         order.deserialize(data)
         self.assertNotEqual( order, None )
-        self.assertNotEqual( order.order_id, 123 )
+        self.assertNotEqual( order.order_id, 1 )
         self.assertEqual( order.customer_id, "321" )
         self.assertEqual( order.order_total, 999 )
-        self.assertEqual( order.order_time, "98765" )
+        self.assertEqual( order.order_time, "98765")
+        self.assertEqual( orders.order_status, 1)
 
 
     def test_deserialize_an_order_with_no_customer_id(self):
-        order = Order()
         data = {"order_id": 0, "order_total": 999, "order_time": "98765", "order_status": 1}
+        order = Order(0)
         self.assertRaises(DataValidationError, order.deserialize, data )
 
     def test_deserialize_an_order_with_no_data(self):
-        order = Order()
+        order = Order(0)
         self.assertRaises(DataValidationError, order.deserialize, None )
 
     def test_deserialize_an_order_with_bad_data(self):
-        order = Order()
+        order = Order(0)
         self.assertRaises( DataValidationError, order.deserialize, "data" )
 
     def test_find_order(self):
-        Order(0, "1", 0, "").save()
-        Order(0, "2", 0, "").save()
+        Order(0, "1", 10, "111").save()
+        Order(0, "2", 100, "222").save()
         order = Order.find(2)
         self.assertIsNot( order, None)
         self.assertEqual( order.order_id, 2 )
         self.assertEqual( order.customer_id, "2")
-        self.assertEqual( order.order_total, 0)
+        self.assertEqual( order.order_total, 100)
+        self.assertEqual( order_time, "222")
 
     def test_find_with_no_orders(self):
         order = Order.find(1)
         self.assertIs( order, None)
 
     def test_order_not_found(self):
-        Order(0, "", 1, "").save()
+        Order(0, "5", 1, "1221").save()
         order = Order.find(2)
         self.assertIs( order, None)
 
