23042a358c1f8d28905899237d70e0c1a04e75c5
mitianyu <markmty1106@hotmail.com>
2017-12-12 09:45:32 -0500

    rewrite bdd

--- features/orders.feature
+++ features/orders.feature
@@ -1,85 +1,54 @@
-Feature: The orders service back-end
-    As a API user
+Feature: The order store service back-end
+    As a Order Store Owner
     I need a RESTful catalog service
-    So that I can keep track of all orders
+    So that I can keep track of all my orders
 
 Background:
     Given the following orders
-        | order_id | customer_id | order_total | order_time | order_status |  
-        |  1       | alpha       | 100         | 06/06/2017 | 1            |
-        |  2       | beta        | 500         | 09/15/2017 | 1            |
-        |  3       | gamma       | 1000        | 04/14/2017 | 1            |
+        | id | name       | time | status |
+        |  1 | fido       | dog      | True      |
+        |  2 | kitty      | cat      | True      |
+        |  3 | leo        | lion     | True      |
 
 Scenario: The server is running
-    When I visit the "home page"
+    When I visit the "Home Page"
     Then I should see "Order Demo RESTful Service" in the title
     And I should not see "404 Not Found"
 
-Scenario: Create an order
+Scenario: Create a Order
     When I visit the "Home Page"
-    And I set the "order_id" to "0"
-    And I set the "customer_id" to "theta"
-    And I set the "order_total" to "200"
-    And I set the "order_time" to "12/11/2017"
-    And I set the "order_status" to "1"
+    And I set the "Name" to "Happy"
+    And I set the "Time" to "Hippo"
     And I press the "Create" button
     Then I should see the message "Success"
 
-#Scenario: List all orders
-#    When I visit "Home Page"
-#    And I press the "Search" button
-#    Then I should see "alpha" in the results
-#    And I should see "beta" in the results
-#    And I should see "gamma" in the results
-#
-#Scenario: Get an order
-#    When I visit the "Home Page"
-#    And I set the "order_id" to "1"
-#    And I press the "Retrieve" button
-#    Then I should see "alpha" in the "customer_id" field
-#
-#Scenario: Update an order
-#    When I visit the "Home Page"
-#    And I set the "order_id" to "1"
-#    And I press the "Retrieve" button
-#    Then I should see "alpha" in the "customer_id" field
-#    And I press the "Update" button
-#    Then I should see the message "Success"
+Scenario: List all orders
+    When I visit the "Home Page"
+    And I press the "Search" button
+    Then I should see "fido" in the results
+    And I should see "kitty" in the results
+    And I should see "leo" in the results
 
+Scenario: List all dogs
+    When I visit the "Home Page"
+    And I set the "Time" to "dog"
+    And I press the "Search" button
+    Then I should see "fido" in the results
+    And I should not see "kitty" in the results
+    And I should not see "leo" in the results
 
-#Scenario: Query for Customer ID
-#    When I attempt to query for "/orders" and search by customer id "alpha"
-#    Then I should see "1"
-#    And I should see "alpha"
-#    And I should see "100"
-#    And I should see "06/06/2017"
-#    And I should see "1"
-#
-#Scenario: Get a order
-#    When I retrieve "/orders" with order id "1"
-#    Then I should see "1"
-#    And I should see "alpha"
-#    And I should see "100"
-#    And I should see "06/06/2017"
-#    And I should see "1"
-#
-#Scenario: Update a order
-#    When I retrieve "/orders" with order id "1"
-#    And I change "order_total" to "200"
-#    And I update "/orders" with order id "1"
-#    Then I should see "200"
-#
-#Scenario: Delete a order
-#    When I visit "/orders"
-#    Then I should see "alpha"
-#    And I should see "beta"
-#    And I should see "gamma"
-#    When I delete "/orders" with order id "3"
-#    And I visit "/orders"
-#    Then I should see "alpha"
-#    And I should see "beta"
-#
-#Scenario: Duplicate an order
-#    When I retrieve "/orders" with order id "1"
-#    And I use "/orders" the order id "1" of that order so the customer can re-order their previous order
-#    Then I should see an order on "/orders" with the same customer_id, order_total, order_time and order_status as the original order
+Scenario: Update a Order
+    When I visit the "Home Page"
+    And I set the "Id" to "1"
+    And I press the "Retrieve" button
+    Then I should see "fido" in the "Name" field
+    When I change "Name" to "Boxer"
+    And I press the "Update" button
+    Then I should see the message "Success"
+    When I set the "Id" to "1"
+    And I press the "Retrieve" button
+    Then I should see "Boxer" in the "Name" field
+    When I press the "Clear" button
+    And I press the "Search" button
+    Then I should see "Boxer" in the results
+    Then I should not see "fido" in the results
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,23 @@
-# run with:
-# python -m unittest discover
-# nosetests --nologcapture
-# nosetests -v --rednose
+# Copyright 2016, 2017 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Order API Service Test Suite
+
+Test cases can be run with the following:
+nosetests -v --with-spec --spec-color
+"""
 
 import unittest
 import logging
@@ -21,130 +37,156 @@ HTTP_409_CONFLICT = 409
 #  T E S T   C A S E S
 ######################################################################
 class TestOrderServer(unittest.TestCase):
+    """ Order Service tests """
 
     def setUp(self):
         self.app = server.app.test_client()
         server.initialize_logging(logging.CRITICAL)
         server.init_db()
         server.data_reset()
-        server.data_load({"customer_id": "2", "order_total": 3, "order_time": "4", "order_status": 1})
-        server.data_load({"customer_id": "6", "order_total": 7, "order_time": "8", "order_status": 1})
-
-    def tearDown(self):
-        server.Order.remove_all()
+        server.data_load({"name": "fido", "time": "dog", "status": True})
+        server.data_load({"name": "kitty", "time": "cat", "status": True})
 
     def test_index(self):
+        """ Test the index page """
         resp = self.app.get('/')
-        self.assertEqual( resp.status_code, HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         self.assertIn('Order Demo REST API Service', resp.data)
 
-
     def test_get_order_list(self):
+        """ Get a list of Orders """
         resp = self.app.get('/orders')
-        self.assertEqual( resp.status_code, HTTP_200_OK )
-        self.assertTrue( len(resp.data) > 0 )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
 
     def test_get_order(self):
-        resp = self.app.get("/orders/2")
+        """ get a single Order """
+        resp = self.app.get('/orders/2')
         #print 'resp_data: ' + resp.data
-        self.assertEqual( resp.status_code, HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertEqual(data["customer_id"], "6")
+        self.assertEqual(data['name'], 'kitty')
 
     def test_get_order_not_found(self):
-        resp = self.app.get('/orders/1000')
+        """ Get a Order that doesn't exist """
+        resp = self.app.get('/orders/0')
         self.assertEqual(resp.status_code, HTTP_404_NOT_FOUND)
         data = json.loads(resp.data)
         self.assertIn('was not found', data['message'])
 
     def test_create_order(self):
-        # save the current number of orders for later comparison
+        """ Create a new Order """
+        # save the current number of orders for later comparrison
         order_count = self.get_order_count()
-        # add a new pet
-        new_order = {"customer_id": "2", "order_total": 3, "order_time": "4", "order_status": 1}
+        # add a new order
+        new_order = {'name': 'sammy', 'time': 'snake', 'status': True}
         data = json.dumps(new_order)
-        resp = self.app.post("/orders", data=data, content_type="application/json")
-        self.assertEqual( resp.status_code, HTTP_201_CREATED )
+        resp = self.app.post('/orders', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_201_CREATED)
         # Make sure location header is set
-        location = resp.headers.get("Location", None)
-        self.assertNotEqual( location, None)
+        location = resp.headers.get('Location', None)
+        self.assertNotEqual(location, None)
         # Check the data is correct
         new_json = json.loads(resp.data)
-        self.assertEqual (new_json["order_id"], 3)
+        self.assertEqual(new_json['name'], 'sammy')
         # check that count has gone up and includes sammy
-        resp = self.app.get("/orders")
+        resp = self.app.get('/orders')
         # print 'resp_data(2): ' + resp.data
         data = json.loads(resp.data)
-        self.assertEqual( resp.status_code, HTTP_200_OK )
-        self.assertEqual( len(data), order_count + 1 )
-        self.assertIn( new_json, data )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        self.assertEqual(len(data), order_count + 1)
+        self.assertIn(new_json, data)
 
     def test_update_order(self):
-        new_order = {"customer_id": "2", "order_total": 5, "order_time": "4", "order_status": 1}
-        data = json.dumps(new_order)
-        resp = self.app.put("/orders/1", data=data, content_type="application/json")
-        self.assertEqual( resp.status_code, HTTP_200_OK )
-        resp = self.app.get("/orders/1", content_type='application/json')
-        self.assertEqual( resp.status_code, HTTP_200_OK )
+        """ Update a Order """
+        new_kitty = {'name': 'kitty', 'time': 'tabby', 'status': True}
+        data = json.dumps(new_kitty)
+        resp = self.app.put('/orders/2', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        resp = self.app.get('/orders/2', content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         new_json = json.loads(resp.data)
-        self.assertEqual (new_json["order_total"], 5)
+        self.assertEqual(new_json['time'], 'tabby')
 
-    def test_update_order_with_nonexisting_order(self):
-        new_order = {"customer_id": "2" ,"order_total" : 3, "order_time": "4", "order_status": 1}
+    def test_update_order_with_no_name(self):
+        """ Update a Order without assigning a name """
+        new_order = {'time': 'dog'}
         data = json.dumps(new_order)
-        resp = self.app.put('/orders/3', data=data, content_type='application/json')
-        self.assertEqual( resp.status_code, HTTP_404_NOT_FOUND )
+        resp = self.app.put('/orders/2', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST)
+
+    def test_update_order_not_found(self):
+        """ Update a Order that doesn't exist """
+        new_kitty = {"name": "timothy", "time": "mouse"}
+        data = json.dumps(new_kitty)
+        resp = self.app.put('/orders/0', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_404_NOT_FOUND)
 
     def test_delete_order(self):
-        # save the current number of pets for later comparrison
+        """ Delete a Order """
+        # save the current number of orders for later comparrison
         order_count = self.get_order_count()
         # delete a order
         resp = self.app.delete('/orders/2', content_type='application/json')
-        self.assertEqual( resp.status_code, HTTP_204_NO_CONTENT )
-        self.assertEqual( len(resp.data), 0 )
+        self.assertEqual(resp.status_code, HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
         new_count = self.get_order_count()
-        self.assertEqual( new_count, order_count - 1)
+        self.assertEqual(new_count, order_count - 1)
 
-    def test_create_order_with_no_customer_id(self):
-        #new_order = {"order_id": 10, "order_total": 100, "order_time": "10112017"}
-        new_order = {"order_total": 100}
+    def test_create_order_with_no_name(self):
+        """ Create a Order without a name """
+        new_order = {'time': 'dog'}
         data = json.dumps(new_order)
         resp = self.app.post('/orders', data=data, content_type='application/json')
-        self.assertEqual( resp.status_code, HTTP_400_BAD_REQUEST )
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST)
 
-    def test_create_order_with_no_order_total(self):
-        new_order = {"customer_id": "50"}
+    def test_create_order_no_content_type(self):
+        """ Create a Order with no Content-Type """
+        new_order = {'time': 'dog'}
         data = json.dumps(new_order)
         resp = self.app.post('/orders', data=data)
-        self.assertEqual( resp.status_code, HTTP_400_BAD_REQUEST )
-
-    def test_create_order_with_no_order_time(self):
-        new_order = {"customer_id": "01","order_total": 100}
-        data = json.dumps(new_order)
-        resp = self.app.post('/orders', data=data, content_type='application/json')
-        self.assertEqual( resp.status_code, HTTP_400_BAD_REQUEST )
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST)
 
     def test_get_nonexisting_order(self):
-        resp = self.app.get("/orders/9")
-        self.assertEqual( resp.status_code, HTTP_404_NOT_FOUND )
+        """ Get a nonexisting Order """
+        resp = self.app.get('/orders/5')
+        self.assertEqual(resp.status_code, HTTP_404_NOT_FOUND)
+
+    def test_call_create_with_an_id(self):
+        """ Call create passing anid """
+        new_order = {'name': 'sammy', 'time': 'snake'}
+        data = json.dumps(new_order)
+        resp = self.app.post('/orders/1', data=data)
+        self.assertEqual(resp.status_code, HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_query_order_list(self):
-        resp = self.app.get("/orders", query_string="customer_id=6")
-        self.assertEqual( resp.status_code, HTTP_200_OK )
-        self.assertTrue( len(resp.data) > 0 )
+        """ Query Orders by time """
+        resp = self.app.get('/orders', query_string='time=dog')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
+        self.assertIn('fido', resp.data)
+        self.assertNotIn('kitty', resp.data)
         data = json.loads(resp.data)
         query_item = data[0]
-        self.assertEqual(query_item["customer_id"], "6")
+        self.assertEqual(query_item['time'], 'dog')
 
-    def test_cancel_an_order(self):
-        resp = self.app.put('/orders/1/cancel')
+    def test_purchase_a_order(self):
+        """ Purchase a Order """
+        resp = self.app.put('/orders/2/purchase', content_type='application/json')
         self.assertEqual(resp.status_code, HTTP_200_OK)
-        data = json.loads(resp.data)
-        self.assertEqual(data["order_status"], 0)
+        resp = self.app.get('/orders/2', content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        order_data = json.loads(resp.data)
+        self.assertEqual(order_data['status'], False)
 
-    def test_cancel_an_order_with_nonexisting_order(self):
-        resp = self.app.put('/orders/3/cancel')
-        self.assertEqual(resp.status_code, HTTP_404_NOT_FOUND)
+    def test_purchase_not_status(self):
+        """ Purchase a Order that is not status """
+        resp = self.app.put('/orders/2/purchase', content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        resp = self.app.put('/orders/2/purchase', content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST)
+        resp_json = json.loads(resp.get_data())
+        self.assertIn('not status', resp_json['message'])
 
 
 ######################################################################
@@ -152,10 +194,9 @@ class TestOrderServer(unittest.TestCase):
 ######################################################################
 
     def get_order_count(self):
-        # save the current number of pets
-        resp = self.app.get("/orders")
-        self.assertEqual( resp.status_code, HTTP_200_OK )
-        # print 'resp_data: ' + resp.data
+        """ save the current number of orders """
+        resp = self.app.get('/orders')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         data = json.loads(resp.data)
         return len(data)
 
